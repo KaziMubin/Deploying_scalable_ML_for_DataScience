a69393c9df85bc036eab225faf01d7b063b93e52
kazimubin356 <kazi.mubin@stud.fra-uas.de>
2024-01-03 14:01:34 +0100

    "Path refferences are edited to fix module not found error"

--- RegressionAsService/MLmodel/views.py
+++ RegressionAsService/MLmodel/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from MLmodel.
+from .models import Trainingdata
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import TrainerSerializer
+
 
 # Create your views here.
-class TrainingModel(APIview):
+class TrainingModel(APIView):
+    def get(self, request):
+        data = Trainingdata.objects.all()
+        serializer = TrainerSerializer(data, many=True)
+        return Response(serializer.data)
+
+
