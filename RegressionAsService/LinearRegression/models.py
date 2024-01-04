62ccf4d6da336c3a808731a854da540b8e6f7195
kazimubin356 <kazi.mubin@stud.fra-uas.de>
2024-01-04 16:43:36 +0100

    "Api post and get view is implemented fully."

--- RegressionAsService/LinearRegression/models.py
+++ RegressionAsService/LinearRegression/models.py
@@ -10,12 +10,12 @@ class Trainingdata(models.Model):
         print(self.user, self.Xtraining, self.ylabels)
 
 
-{
-    "Xtraining" : [100,200,300,400,500,600,700,800,900],
-    "ylabels" : [911,811,711,611,511,411,311,211,111],
-    "user" : "Mubin"
-}
-
-{
-    "user" : "Mubin"
-}
+# {
+#     "Xtraining" : [100,200,300,400,500,600,700,800,900],
+#     "ylabels" : [911,811,711,611,511,411,311,211,111],
+#     "user" : "Mubin"
+# }
+#
+# {
+#     "user" : "Mubin"
+# }
